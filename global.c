fd687d8d5a3244d29d79fb04cb815f62b1d71cc3
Jinyi Xia <jinyi.xia@bupt.edu.cn>
2024-05-27 01:23:21 +0800

    First commit.

---
+++ global.c
@@ -0,0 +1,6 @@
+
+#include "global.h"
+
+PFLT_FILTER gFilterHandle = NULL;
+PWCHAR pPathPrefix = L"\\Device\\HarddiskVolume3\\FsFilterDemo\\";
+PCHAR rc4_key = "12345";
---
+++ global.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef __GLOBAL__
+#define __GLOBAL__
+
+#include <fltKernel.h>
+
+#define MY_TAG (2021212057UL)
+
+extern PFLT_FILTER gFilterHandle;
+extern PWCHAR pPathPrefix;
+extern PCHAR rc4_key;
+
+#endif
